0a3ab7e8fb4138d1da84538622e983286498bc0d
Everton Lima Aleixo <everton.jiujitsu@gmail.com>
2017-12-01 11:33:59 -0200

    Fixing add_loc script.

--- add_loc.py
+++ add_loc.py
@@ -6,11 +6,41 @@
 #                                                                                       #
 #########################################################################################
 from random import uniform
+import sys
 
 def newpoint():
    return uniform(-51,-45), uniform(-19, -15)
 
-points = (newpoint() for x in range(10))
-for point in points:
-   print(point)
+DEBUG__ = False
 
+file = sys.argv[1]
+file_out = "ratings_with_latlng.csv"
+count = 0
+
+f_out = open(file_out, 'w')
+f = open(file, 'r')
+
+lines = f.readlines()
+positions = dict()
+
+for line in lines:
+    line_str = line.rstrip('\n')
+    usr_id = line.split(",")[0]
+
+    if count == 0:
+        line_str += ",lat,lng"
+    else:
+        loc = newpoint()
+        if usr_id in positions:
+            loc = positions[usr_id]
+        else:
+            positions[usr_id] = loc
+        line_str += "," + str(loc[0])  + "," + str(loc[1])
+
+    count = 1
+    f_out.write(line_str+"\n")
+    if DEBUG__:
+        print(line_str)
+
+f_out.close()
+f.close()
